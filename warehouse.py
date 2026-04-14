68529d2c9374f8a12f298ac16f6b759aaf6adb7f
longnvb24 <longnvb24@gmail.com>
2026-04-15 07:33:32 +0800

    update warehouse.py

---
+++ warehouse.py
@@ -0,0 +1,6 @@
+class Rides:
+    def __init__(self, ride_id, origin_position, state, target_position):
+        self.ride_id = ride_id
+        self.origin_position = origin_position
+        self.state = state
+        self.target_position = target_position
